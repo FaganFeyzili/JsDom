a682b50d86efd6207e18ec5837590464a1747b39
FaganFeyzili <feyzilifeqan@gmail.com>
2026-07-14 17:54:06 +0000

    commit

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
 
+
+$('.btn').click(function(e){
+    e.preventDefault();
+    
+    $('.btn').html('runned');
+});
+
+
+
+$('.btn').click(function(e){
+    e.preventDefault();
+     
+    let func = (a) => {
+        for(let i=1; i<=a; i++){
+            $('.col-md-12').append(`<h2>${i}</h2>`)
+        }
+    }
+    func($('input').val())
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
